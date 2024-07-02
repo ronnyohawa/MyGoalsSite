2e7773fd7cb3a93ba7af487b60fa38ded36084b9
Ronny <ochiengohawa@gmail.com>
2024-07-02 17:36:01 +0300

    second manipulation

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-let nowday = new Date()
-const days=["sunday","monday","tuesday","wenseday","thursday","friday","saturday"]
-document.getElementById('day').innerHTML = days[nowday.getUTCDay()];
-function getTime(){
-    let nowTime =new Date();
-    document.getElementById("hour").innerHTML = nowTime.getUTCHours()
-    document.getElementById("min").innerHTML =nowTime.getUTCMinutes()
-    document.getElementById("sec").innerHTML = nowTime.getUTCSeconds()
-}
-getTime()
+function updateTime() {
+    const currentTime = new Date().toUTCString().slice(-12, -4);  // Extract only the time part
+    document.getElementById('current-time').textContent = currentTime;
+  
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDay = days[new Date().getUTCDay()];
+    document.getElementById('current-day').textContent = currentDay;
+  }
+  
+  updateTime();
+  setInterval(updateTime, 1000);
+  
